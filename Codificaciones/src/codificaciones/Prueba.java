5ff7284114db6ca1aa35ab2b17ae1aca2d0ad62d
TLunaaa <42612882+TLunaaa@users.noreply.github.com>
2018-09-16 22:48:00 -0300

    Se agrego el ejercicio 20 y mitad del 25

--- Codificaciones/src/codificaciones/Prueba.java
+++ Codificaciones/src/codificaciones/Prueba.java
@@ -9,37 +9,13 @@ public class Prueba {
         Alfabeto fuente = new Alfabeto();
         Alfabeto codigo = new Alfabeto();
         CodigoBloque codigobloque = new CodigoBloque(fuente,codigo);
-        Palabra x1 = new Palabra();
-        Palabra x2 = new Palabra();
-        Palabra x3 = new Palabra();
-        Palabra x4 = new Palabra();
-        Palabra x5 = new Palabra();
+        Palabra x1 = new Palabra(1,1,0);
+        Palabra x2 = new Palabra(0,1);
+        Palabra x3 = new Palabra(1,1,1);
+        Palabra x4 = new Palabra(0,0);
+        Palabra x5 = new Palabra(1,0);
         Palabra x6 = new Palabra();
-        
-        x1.addSimbolo("0");
-        x1.addSimbolo("0");
-        x1.addSimbolo("0");
-        
-        x2.addSimbolo("0");
-        x2.addSimbolo("0");
-        x2.addSimbolo("1");
-        
-        x3.addSimbolo("0");
-        x3.addSimbolo("1");
-        x3.addSimbolo("0");
-        
-        x4.addSimbolo("0");
-        x4.addSimbolo("1");
-        x4.addSimbolo("1");
-        
-        x5.addSimbolo("1");
-        x5.addSimbolo("0");
-        x5.addSimbolo("0");
-        
-        x6.addSimbolo("1");
-        x6.addSimbolo("0");
-        x6.addSimbolo("1");
-        
+                
         codigo.addSimbolos("0");
         codigo.addSimbolos("1");
         
@@ -48,16 +24,26 @@ public class Prueba {
         fuente.addSimbolos("s3");
         fuente.addSimbolos("s4");
         fuente.addSimbolos("s5");
-        fuente.addSimbolos("s6");
+        //fuente.addSimbolos("s6");
         
         codigobloque.addPalabra(x1);
         codigobloque.addPalabra(x2);
         codigobloque.addPalabra(x3);
         codigobloque.addPalabra(x4);
         codigobloque.addPalabra(x5);
-        codigobloque.addPalabra(x6);
+        codigobloque.addProb(0.4);
+        codigobloque.addProb(0.12);
+        codigobloque.addProb(0.25);
+        codigobloque.addProb(0.08);
+        codigobloque.addProb(0.15);
         
-        System.out.println(codigobloque.cumpleCodigoBloque());
-        System.out.println(codigobloque.esNoSingular());
+        //codigobloque.addPalabra(x6);
+        
+        System.out.println("La codificacion cumple con las propiedades de codigo bloque: " +codigobloque.cumpleCodigoBloque());
+        System.out.println("La codificacion es no singular: " + codigobloque.esNoSingular());
+        System.out.println("Desigualdad de Kraft: " +codigobloque.cumpleDesigualdad());
+        System.out.println("La codificacion es instantanea: " +codigobloque.esInstantaneo());
+        System.out.println("Longitud media del codigo: " +codigobloque.calcularLongitudMedia());
+        System.out.println("Entriopia:" + codigobloque.calcularEntropia());
     }
 }
